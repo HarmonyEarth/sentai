77d1bec89ddc539b752d2dfbaceac02de789d896
HarmonyEarth <sentai@chowa.dev>
2022-01-27 14:22:45 -0500

    Hero Banner

--- src/components/Series/HeroBanner.tsx
+++ src/components/Series/HeroBanner.tsx
@@ -1,9 +1,35 @@
-import { Grid } from "@mui/material";
+import { Grid, useMediaQuery, useTheme } from "@mui/material";
 import React from "react";
 import HeroCard from "./HeroCard";
-
+const list: number[] = [];
+for (let i = 0; i < 10; i++) {
+  list.push(i);
+}
+const img1 =
+  "https://static.wikia.nocookie.net/powerrangers/images/6/63/Gokai-redsilver.png";
+const img2 =
+  "https://static.wikia.nocookie.net/powerrangers/images/5/58/Kirama-green.png";
 const HeroBanner = () => {
-  return <Grid container></Grid>;
+  const theme = useTheme();
+  const mobile = useMediaQuery(theme.breakpoints.down("sm"));
+  return (
+    <Grid container>
+      <Grid container item xs={12} md={4}>
+        {list.slice(0, 3).map(() => (
+          <Grid container item xs={4} sm={4}>
+            <HeroCard heroImageUrl={img1} />
+          </Grid>
+        ))}
+      </Grid>
+      <Grid container item xs={12} md={8}>
+        {list.slice(mobile ? -4 : 4).map(() => (
+          <Grid container item xs={3} sm={2}>
+            <HeroCard heroImageUrl={img2} />
+          </Grid>
+        ))}
+      </Grid>
+    </Grid>
+  );
 };
 
 export default HeroBanner;
--- src/styles/Series/HeroCard.styles.ts
+++ src/styles/Series/HeroCard.styles.ts
@@ -1,1 +1,20 @@
-export {};
+import styled from "styled-components";
+
+interface Props {
+  randomColor: string;
+  randomColor2: string;
+  heroImageUrl: string;
+}
+
+export const SenshiCard = styled.div<Props>`
+  background: url(${(props) => props.heroImageUrl}),
+    linear-gradient(
+      ${Math.random()}turn,
+      ${(props) => props.randomColor},
+      ${(props) => props.randomColor2}
+    );
+  background-repeat: no-repeat;
+  background-position: center top;
+  height: 400px;
+  width: 100%;
+`;
