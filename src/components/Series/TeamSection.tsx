e5f49b344e3c9aa2a9bdd546a790349e6171cf43
HarmonyEarth <sentai@chowa.dev>
2022-01-27 15:49:35 -0500

    Homepage Team Section

--- src/components/Series/TeamSection.tsx
+++ src/components/Series/TeamSection.tsx
@@ -1,7 +1,33 @@
+import { Grid } from "@mui/material";
 import React from "react";
+import TeamCard from "./TeamCard";
 
+const list: number[] = [];
+for (let i = 0; i < 11; i++) {
+  list.push(i);
+}
+
+const symbol =
+  "https://static.wikia.nocookie.net/powerrangers/images/4/4a/Icon-turboranger.png";
+const shortTeamName = "Turboranger";
+const year = 1989;
+const logo =
+  "https://static.wikia.nocookie.net/powerrangers/images/a/a0/Logo-turboranger.png";
 const TeamSection = () => {
-  return <div></div>;
+  return (
+    <Grid container>
+      {list.map(() => (
+        <Grid item xs={12} sm={6} md={4}>
+          <TeamCard
+            logo={logo}
+            symbol={symbol}
+            year={year}
+            shortTeamName={shortTeamName}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
 };
 
 export default TeamSection;
--- src/styles/Series/TeamCard.styles.ts
+++ src/styles/Series/TeamCard.styles.ts
@@ -1,1 +1,52 @@
-export {};
+import styled from "styled-components";
+
+interface Props {
+  randomColor: string;
+}
+
+export const SeriesCard = styled.div<Props>`
+  max-width: 100%;
+  height: 120px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background: linear-gradient(
+    90deg,
+    black 50%,
+    ${(props) => props.randomColor}
+  );
+`;
+
+export const SeriesLeftSide = styled.div`
+  width: 150px;
+  height: 100px;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+`;
+
+export const SeriesYearText = styled.h1`
+  font-size: 20px;
+  z-index: 1;
+  margin-right: 3px;
+`;
+
+export const SeriesTeamText = styled.h1`
+  font-size: 20px;
+  z-index: 1;
+`;
+
+export const SeriesSymbol = styled.img`
+  position: absolute;
+  width: 100px;
+  height: auto;
+  opacity: 0.5;
+  filter: grayscale(1);
+`;
+
+export const SeriesLogo = styled.img`
+  max-width: 150px;
+  height: 100px;
+`;
