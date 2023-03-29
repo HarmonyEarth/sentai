e05f14a385d79e4bebde475b6029da2b3a84303c
HarmonyEarth <sentai@chowa.dev>
2023-03-28 22:33:25 -0400

    Fix Firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -16,21 +16,12 @@ import {
 import { getStorage } from 'firebase/storage';
 
 export const firebaseConfig = {
-<<<<<<< HEAD
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENGER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-=======
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENGER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
->>>>>>> 8a11afeea1f22a5a47baf4984b3cdacc0480a9ad
 };
 
 // Initialize Firebase
