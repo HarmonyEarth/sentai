cfe4f40b4a58f42202b1cc04d38f2f8939541e1f
HarmonyEarth <89561014+HarmonyEarth@users.noreply.github.com>
2024-08-07 09:17:14 -0400

    Update types and resolve build issues

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,9 @@
 export type FileState = File | Blob | MediaSource | String | undefined;
 
 export interface Team {
-  shortTeamName: string;
-  fullTeamNameEN: string;
-  fullTeamNameJP: string;
+  shortTeamName: string; //Short / common team Name in English
+  fullTeamNameEN: string; //Full Team Name in English
+  fullTeamNameJP: string; //Full Team Name in Japanese
   year: string;
   logo: FileState;
   symbol: FileState;
@@ -14,20 +14,20 @@ export interface Team {
 export interface Member {
   heroId: string;
   color: string;
-  position: number | string;
-  heroNameEN1: string;
-  heroNameEN2: string;
-  heroNameJP1: string;
-  heroNameJP2: string;
-  heroImage1: FileState;
-  heroImage2: FileState;
-  heroImage3: FileState;
-  heroImage4: FileState;
-  heroHelmet: FileState;
-  heroSymbol: FileState;
+  position: number | string; //Position on the team
+  heroNameEN1: string; //Hero - Civilian Name English
+  heroNameEN2: string; //Hero  - Hero Name English
+  heroNameJP1: string; //Hero  - Civilian Name Japanese
+  heroNameJP2: string; //Hero  - Hero Name Japanese
+  heroImage1: FileState; //Hero Background
+  heroImage2: FileState; //Full Body Front Profile Hero
+  heroImage3: FileState; //Hero - Primary Civilian Form
+  heroImage4: FileState; //Hero - Primary Hero Form
+  heroHelmet: FileState; //Hero Helmet
+  heroSymbol: FileState; //Hero Symbol
   teamId: string;
   id: string;
-  locationEN: string;
-  locationJP: string;
-  locationImage: string;
+  locationEN: string; //Location of the English Name in Hero Content
+  locationJP: string; //Location of the Japanese Name in Hero Content
+  locationImage: string; //Location of the Image in Hero Content
 }
